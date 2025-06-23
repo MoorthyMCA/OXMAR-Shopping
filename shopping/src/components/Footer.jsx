40938d673bc7d48cb48dcaad54db256d98c124b5
Thala Dev <thala.dev@example.com>
2025-06-23 22:55:59 +0530

    feat: updated image and responsive fixes

--- shopping/src/components/Footer.jsx
+++ shopping/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
           {/* About Section */}
           <div className="col-md-4 mb-3">
-            <h5>About OXMAR</h5>
+            <h5>About <span className='text-warning'>OXMAR</span> </h5>
             <p>
               At OXMAR, fashion is self-expression. We blend trend, elegance, and comfort to bring you handpicked styles that reflect your unique identity.
             </p>
@@ -16,21 +16,23 @@ const Footer = () => {
 
           {/* Quick Links */}
           <div className="col-md-4 mb-3">
-            <h5>Quick Links</h5>
+            <h5><i class="bi bi-box-arrow-up-left"></i> Quick Links</h5>
             <ul className="list-unstyled">
-              <li><a href="/" className="text-white text-decoration-none">Home</a></li>
-              <li><a href="/about" className="text-white text-decoration-none">About</a></li>
-              <li><a href="/products" className="text-white text-decoration-none">Products</a></li>
-              <li><a href="/contact" className="text-white text-decoration-none">Contact</a></li>
+              <li><a href="/" className="text-white text-decoration-none"><i class="bi bi-house text-danger"></i> Home</a></li>
+              {/* <li><a href="/about" className="text-white text-decoration-none"> About</a></li> */}
+              
+              <li><a href="/contact" className="text-white text-decoration-none"><i class="bi bi-file-earmark-person text-success"></i> About</a></li>
+
+              <li><a href="/products" className="text-white text-decoration-none"><i class="bi bi-basket text-warning"></i> Products</a></li>
             </ul>
           </div>
 
           {/* Contact Info */}
           <div className="col-md-4 mb-3">
-            <h5>Contact Us</h5>
-            <p>Email: oxmarshopping@outlook.com</p>
-            <p><i class="bi bi-telephone-outbound-fill"></i>   Phone: +91 9952653016</p>
-            <p>Location: Dindigul, India</p>
+            <h5><i class="bi bi-person-lines-fill text-primary"></i> Contact Us</h5>
+            <p><i class="bi bi-envelope-at-fill text-warning"></i> Email: oxmarshopping@outlook.com</p>
+            <p><i class="bi bi-telephone-outbound-fill text-secondary"></i>   Phone: +91 9952653016</p>
+            <p><i class="bi bi-geo-alt text-success"></i> Location: Dindigul, India</p>
           </div>
 
         </div>
@@ -39,7 +41,7 @@ const Footer = () => {
           {/* <p className="mb-0">&copy; {new Date().getFullYear()} OXMAR Shopping. All rights reserved.</p> */}
 
           <p className="mb-0">
-  &copy; {new Date().getFullYear()} OXMAR Shopping. All rights reserved. <br />
+  &copy; {new Date().getFullYear()} <span className='text-warning'>OXMAR Shopping.</span> All rights reserved. <br />
   Designed & Developed by Thatchana Moorthy.
 </p>
 
